ecf8edd172661ea9892b82e97de31395e1c8ab1d
Mikhail Ryabchikov <Nolifegonnamad@gmail.com>
2025-09-22 16:31:36 +0400

    code refactored
    code refactored

---
+++ BankSimulator/Money.cs
@@ -0,0 +1,33 @@
+﻿namespace BankSimulator
+{
+    public class Money
+    {
+        public string Currency { get; private set; }
+        public decimal Balance { get; private set; }
+
+        public Money(string currency, decimal initialBalance)
+        {
+            Currency = currency;
+            Balance = initialBalance;
+        }
+
+        public void DepositMoney(decimal amount)
+        {
+            if (amount > 0)
+            {
+                Balance += amount;
+            }
+        }
+
+        public bool WithdrawMoney(decimal amount)
+        {
+            if (amount <= 0 || amount > Balance)
+            {
+                return false;
+            }
+
+            Balance -= amount;
+            return true;
+        }
+    }
+}
